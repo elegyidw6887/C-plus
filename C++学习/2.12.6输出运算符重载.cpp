a95cf68f8ab275f6a53aded55ec505757329121f
LOST <654725797@qq.com>
2021-03-23 13:13:50 +0800

    2021.3.23 13:13

---
+++ C++学习/2.12.6输出运算符重载.cpp
@@ -0,0 +1,31 @@
+/*
+输出运算符重载
+*/
+
+#include<iostream>
+using namespace std;
+
+class CExample
+{
+public:
+
+	CExample()
+	{
+
+	}
+
+};
+
+int main()
+{
+	/*
+	
+	输出运算符重载
+		
+	
+	*/
+
+
+	system("pause");
+	return 0;
+}
