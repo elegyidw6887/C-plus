0c96140b8dc3d167e64a5095fcdb62d792dbc722
LOST <654725797@qq.com>
2021-03-23 13:34:00 +0800

    2021.3.23 13:33

--- C++学习/2.12.6输出运算符重载.cpp
+++ C++学习/2.12.6输出运算符重载.cpp
@@ -2,30 +2,50 @@
 输出运算符重载
 */
 
-#include<iostream>
-using namespace std;
-
-class CExample
-{
-public:
-
-	CExample()
-	{
-
-	}
-
-};
-
-int main()
-{
-	/*
-	
-	输出运算符重载
-		
-	
-	*/
-
-
-	system("pause");
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//class CExample
+//{
+//private:
+//
+//	int cAge;
+//
+//public:
+//
+//	CExample(int age)
+//	{
+//		cAge = age;
+//	}
+//
+//	friend ostream& operator<<(ostream& os, const CExample& ce);
+//
+//};
+//
+////void operator<<(ostream& os, const CExample& ce)
+////{
+////	os << ce.cAge << endl;
+////}
+//
+//ostream& operator<<(ostream& os, const CExample& ce)
+//{
+//	os << ce.cAge;
+//	return os;
+//}
+//
+//int main()
+//{
+//	/*
+//	
+//	输出运算符重载
+//		用于类的输出，无返回值的无法进行连续输出，有返回值的可以进行连续输出
+//		对于类内的私有成员，可以使用友元来进行重载
+//	
+//	*/
+//
+//	CExample ce1(23);
+//	cout << ce1;
+//
+//	system("pause");
+//	return 0;
+//}
