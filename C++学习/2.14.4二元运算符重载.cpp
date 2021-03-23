124ef9febff7489042fdb670157317081ccc73ae
LOST <654725797@qq.com>
2021-03-23 13:00:39 +0800

    2021.3.23 13:00

---
+++ C++学习/2.14.4二元运算符重载.cpp
@@ -0,0 +1,53 @@
+/*
+二元运算符重载
+*/
+
+//#include<iostream>
+//using namespace std;
+//
+//class CExample
+//{
+//public:
+//
+//	int cAge;
+//
+//	CExample(int age)
+//	{
+//		cAge = age;
+//	}
+//
+//};
+//
+//int operator>=(CExample& ce1, CExample& ce2)
+//{
+//	return (ce1.cAge >= ce2.cAge);
+//}
+//
+//int main()
+//{
+//	/*
+//	
+//	二元运算符重载
+//
+//		一元运算符：一次对一个数据进行操作
+//		二元运算符：一次对两个数据进行操作
+//		三元运算符：一次对三个数据进行操作
+//
+//		二元运算符：
+//			算术运算符：
+//				+、-、*、/
+//			关系运算符：
+//				>、<、>=、<=、=
+//			位运算符：
+//				^、&、|
+//	
+//	*/
+//	CExample ce1(25);
+//	CExample ce2(23);
+//
+//	cout << (ce1 >= ce2) << endl;
+//
+//
+//	system("pause");
+//	return 0;
+//}
