1fd80e91664c00610fb3d636d57736e49c436b53
LOST <654725797@qq.com>
2021-04-01 14:20:18 +0800

    2021.4.1 14:20

---
+++ C++学习/2.13.1继承.cpp
@@ -0,0 +1,61 @@
+/*
+继承
+*/
+
+//#include<iostream>
+//using namespace std;
+//
+//class CFundation//父类
+//{
+//public:
+//
+//	void Study()
+//	{
+//		cout << "Study" << endl;
+//	}
+//
+//};
+//
+//class CChild:public CFundation//子类
+//{
+//public:
+//
+//	void GoToSchool()
+//	{
+//		cout << "Go to school" << endl;
+//	}
+//
+//};
+///*	1
+//class CFundation
+//被继承类：基类、父类
+//class CChild:public CFundation
+//继承类：派生类、子类
+//*/
+//
+//class CMan:public CFundation//子类
+//{
+//public:
+//
+//	void GoToWork()
+//	{
+//		cout << "Go to work" << endl;
+//	}
+//};
+//
+//int main()
+//{
+//	/*
+//	
+//	继承：
+//		在多个类存在相同代码部分的时候，可以使用继承来减少整体的重复代码的量
+//		继承的方法如上所示
+//
+//		多继承：
+//			如果要继承多个类，可以在类名称之间添加逗号来继承多个类
+//	*/
+//
+//
+//	system("pause");
+//	return 0;
+//}
