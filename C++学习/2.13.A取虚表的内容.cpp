5131a834d4bf12842bae41325ea82f614a907510
LOST <654725797@qq.com>
2021-04-02 15:45:26 +0800

    2021.4.2 15:45

---
+++ C++学习/2.13.A取虚表的内容.cpp
@@ -0,0 +1,45 @@
+/*
+取虚表的内容
+*/
+
+//#include<iostream>
+//using namespace std;
+//
+//class CFather
+//{
+//public:
+//
+//	virtual void Show()
+//	{
+//		cout << "FatherClass" << endl;
+//	}
+//};
+//
+//class CChild :public CFather
+//{
+//public:
+//
+//	void Show()
+//	{
+//		cout << "SonClass" << endl;
+//	}
+//};
+//
+//int main()
+//{
+//	/*
+//	
+//	好家伙，这部分内容感觉用不上，不学了先
+//	
+//	*/
+//
+//	CFather* cfa1 = new CChild;
+//	cfa1->Show();
+//
+//	cout << &cfa1 << endl;
+//
+//	delete(cfa1);
+//
+//	system("pause");
+//	return 0;
+//}
