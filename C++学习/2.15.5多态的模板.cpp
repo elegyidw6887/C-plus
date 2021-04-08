db4705e2d2a466de510ec45ab4f14ff02a5be837
LOST <654725797@qq.com>
2021-04-08 20:57:44 +0800

    2021.4.8 20:57

--- C++学习/2.15.5多态的模板.cpp
+++ C++学习/2.15.5多态的模板.cpp
@@ -2,19 +2,48 @@
 多态的模板
 */
 
-#include<iostream>
-using namespace std;
-
-int main()
-{
-	/*
-	
-	多态的模板
-		
-	
-	*/
-
-
-	system("pause");
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//template<typename CF1>
+//class CFather
+//{
+//public:
+//
+//	CF1 i;
+//
+//	virtual void Show()
+//	{
+//		cout << "FatherClass" << endl << i << endl;
+//	}
+//};
+//
+//template<typename CS1,typename CS2,typename CS3>
+//class CSon :public CFather<CS1>
+//{
+//public:
+//
+//	CS1 i;
+//
+//	virtual void Show()
+//	{
+//		cout << "SonClass" << endl << i << endl;
+//	}
+//};
+//
+//int main()
+//{
+//	/*
+//	
+//	多态的模板
+//		(父类在使用模板之后，在后续任何出现父类名称的地方，必须要给父类使用<>来传递参数的类型，传递的个数由模板声明时的个数决定)
+//	
+//	*/
+//
+//	CFather<int>* cf1 = new CSon<int,char,double>;
+//	cf1->Show();
+//
+//
+//	system("pause");
+//	return 0;
+//}
