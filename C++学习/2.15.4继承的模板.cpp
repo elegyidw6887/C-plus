e095eeaa1db581122ce50c5dccdf3e18b0a6053e
LOST <654725797@qq.com>
2021-04-08 20:43:12 +0800

    2021.4.8 20:43

--- C++学习/2.15.4继承的模板.cpp
+++ C++学习/2.15.4继承的模板.cpp
@@ -2,53 +2,60 @@
 继承的模板
 */
 
-#include<iostream>
-using namespace std;
-
-template<typename T>
-class CFather
-{
-public:
-
-	T i;
-	CFather(T a)
-	{
-		i = a;
-	}
-
-	void Show()
-	{
-		cout << i << endl;
-	}
-
-};
-
-template<typename T>
-class CSon :public CFather<T>
-{
-public:
-
-	T i;
-	CSon(T a) :CFather<T>(23)
-	{
-		i = a;
-	}
-
-};
-
-int main()
-{
-	/*
-	
-	继承的模板
-		在继承中，子类的继承处的CFather也需要添加模板参数列表
-	
-	*/
-
-	CSon<int> cs1(25);
-	cs1.Show();
-
-
-	system("pause");
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//template<typename T>
+//class CFather
+//{
+//public:
+//
+//	T i;
+//	CFather(T a)
+//	{
+//		i = a;
+//	}
+//
+//	void Show()
+//	{
+//		cout << i << endl;
+//	}
+//
+//};
+//
+//template<typename T>
+//class CSon :public CFather<T>
+//{
+//public:
+//
+//	T i;
+//	CSon(T a) :CFather<T>(23)
+//	{
+//		i = a;
+//	}
+//
+//	void ShowAgain()
+//	{
+//		cout << i << endl;
+//	}
+//
+//};
+//
+//int main()
+//{
+//	/*
+//	
+//	继承的模板
+//		在继承中，子类的继承处的CFather也需要添加模板参数列表
+//	
+//	*/
+//
+//	CSon<int> cs1(25);
+//	//创建的时候传递参数类型
+//	cs1.Show();
+//	cs1.ShowAgain();
+//
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ C++学习/2.15.5多态的模板.cpp
@@ -0,0 +1,20 @@
+/*
+多态的模板
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	/*
+	
+	多态的模板
+		
+	
+	*/
+
+
+	system("pause");
+	return 0;
+}
